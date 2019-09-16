0ee2e38875790aed64737c1a74962f37559cb73c
B707H <tothbalint90@gmail.com>
2019-09-16 17:01:09 +0200

    a6

--- src/Main.java
+++ src/Main.java
@@ -2,24 +2,16 @@ public class Main {
 
   public static void main(String[] args) {
 
-<<<<<<< HEAD
     greet("Hello ", "Balint!");
-
-  }
-
-  public static void greet(String firstParam, String secondParam){
-    System.out.println(firstParam + secondParam);
-  }
-
-
-=======
     cheer();
 
+  }
+
+  public static void greet(String firstParam, String secondParam) {
+    System.out.println(firstParam + secondParam);
   }
 
   private static void cheer() {
     System.out.println("Agrabah!");
   }
-
->>>>>>> a7a785f2bb1e97a3ab564118546fb5141103292b
 }
