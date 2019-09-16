14cf26e5825b67bd2c52d0c2b46e579d653eb8eb
B707H <tothbalint90@gmail.com>
2019-09-16 16:26:44 +0200

    a5

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,13 @@ public class Main {
 
   public static void main(String[] args) {
 
-    
+    greet("Hello ", "Balint!");
 
   }
+
+  public static void greet(String firstParam, String secondParam){
+    System.out.println(firstParam + secondParam);
+  }
+
 
 }
