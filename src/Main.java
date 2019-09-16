26c76b3911d4611a71a8a4430ee25d865219d524
KrisztinaFoldi <krisztifoldi11@gmail.com>
2019-09-16 16:30:53 +0200

    b2

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,13 @@
 public class Main {
 
   public static void main(String[] args) {
-
+    System.out.println(count(23));
     
 
+  }
+
+  private static int count(Integer number) {
+    return number;
   }
 
 }
